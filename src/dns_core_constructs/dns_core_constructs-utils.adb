305ce4ed8620c7e55f14af8eb883f7d7ff558739
Michael Casadevall <michael@casadevall.pro>
2018-11-19 12:43:39 -0500

    Fix a spinloop in the sending thread, clear up most of the memory leaks, keep going
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

---
+++ src/dns_core_constructs/dns_core_constructs-utils.adb
@@ -0,0 +1,10 @@
+package body DNS_Core_Constructs.Utils is
+   procedure Free_Raw_Packet_Record_Ptr(Ptr: in out Raw_Packet_Record_Ptr) is
+      procedure Free_Ptr_Record is new Ada.Unchecked_Deallocation(Object => Raw_Packet_Record,
+                                                        Name   => Raw_Packet_Record_Ptr);
+   begin
+      Free_Stream_Element_Array_Ptr(Ptr.Raw_Data.Data);
+      Free_Ptr_Record(Ptr);
+   end Free_Raw_Packet_Record_Ptr;
+   
+end DNS_Core_Constructs.Utils;
