a4e58f11b621508cc048b626322e3412e36eddb7
Michael Casadevall <michael@casadevall.pro>
2019-07-13 20:43:41 -0400

    Clear up formatting in DNS core constructs
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- src/dns_core_constructs/dns_core_constructs.ads
+++ src/dns_core_constructs/dns_core_constructs.ads
@@ -93,22 +93,94 @@ package DNS_Core_Constructs is
       DOA,
       TA,
       DLV);
-   for RR_Types use (A => 1, NS => 2, MD => 3, MF => 4, CNAME => 5, SOA => 6,
-      MB => 7, MG => 8, MR => 9, DNS_NULL => 10, WKS => 11, PTR => 12,
-      HINFO => 13, MINFO => 14, MX => 15, TXT => 16, RP => 17, AFSDB => 18,
-      X25 => 19, ISDN => 20, RT => 21, NSAP => 22, NSAP_PTR => 23, SIG => 24,
-      KEY => 25, PX => 26, GPOS => 27, AAAA => 28, LOC => 29, NXT => 30,
-      EID => 31, NIMLOC => 32, SRV => 33, ATMA => 34, NAPTR => 35, KX => 36,
-      CERT => 37, A6 => 38, DNAME => 39, SINK => 40, OPT => 41, APL => 42,
-      DS => 43, SSHFP => 44, IPSECKEY => 45, RRSIG => 46, NSEC => 47,
-      DNSKEY => 48, DHCID => 49, NSEC3 => 50, NSEC3PARAM => 51, TLSA => 52,
-      SMIMEA           => 53, HIP => 55, NINFO => 56, RKEY => 57, TALINK => 58,
-      CDS => 59, CDNSKEY => 60, OPENPGPKEY => 61, CSYNC => 62, SPF => 99,
-      UINFO => 100, UID => 101, GID => 102, UNSPEC => 103, NID => 104,
-      L32 => 105, L64 => 106, LP => 107, EUI48 => 108, EUI64 => 109,
-      TKEY => 249, TSIG => 250, IXFR => 251, AXFR => 252, MAILB => 253,
-      MAILA => 254, WILDCARD => 255, URI => 256, CAA => 257, AVC => 258,
-      DOA              => 259, TA => 32768, DLV => 32769);
+
+   --!pp off
+   for RR_Types use (A => 1,
+                     NS => 2,
+                     MD => 3,
+                     MF => 4,
+                     CNAME => 5,
+                     SOA => 6,
+                     MB => 7,
+                     MG => 8,
+                     MR => 9,
+                     DNS_NULL => 10,
+                     WKS => 11,
+                     PTR => 12,
+                     HINFO => 13,
+                     MINFO => 14,
+                     MX => 15,
+                     TXT => 16,
+                     RP => 17,
+                     AFSDB => 18,
+                     X25 => 19,
+                     ISDN => 20,
+                     RT => 21,
+                     NSAP => 22,
+                     NSAP_PTR => 23,
+                     SIG => 24,
+                     KEY => 25,
+                     PX => 26,
+                     GPOS => 27,
+                     AAAA => 28,
+                     LOC => 29,
+                     NXT => 30,
+                     EID => 31,
+                     NIMLOC => 32,
+                     SRV => 33,
+                     ATMA => 34,
+                     NAPTR => 35,
+                     KX => 36,
+                     CERT => 37,
+                     A6 => 38,
+                     DNAME => 39,
+                     SINK => 40,
+                     OPT => 41,
+                     APL => 42,
+                     DS => 43,
+                     SSHFP => 44,
+                     IPSECKEY => 45,
+                     RRSIG => 46,
+                     NSEC => 47,
+                     DNSKEY => 48,
+                     DHCID => 49,
+                     NSEC3 => 50,
+                     NSEC3PARAM => 51,
+                     TLSA => 52,
+                     SMIMEA => 53,
+                     HIP => 55,
+                     NINFO => 56,
+                     RKEY => 57,
+                     TALINK => 58,
+                     CDS => 59,
+                     CDNSKEY => 60,
+                     OPENPGPKEY => 61,
+                     CSYNC => 62,
+                     SPF => 99,
+                     UINFO => 100,
+                     UID => 101,
+                     GID => 102,
+                     UNSPEC => 103,
+                     NID => 104,
+                     L32 => 105,
+                     L64 => 106,
+                     LP => 107,
+                     EUI48 => 108,
+                     EUI64 => 109,
+                     TKEY => 249,
+                     TSIG => 250,
+                     IXFR => 251,
+                     AXFR => 252,
+                     MAILB => 253,
+                     MAILA => 254,
+                     WILDCARD => 255,
+                     URI => 256,
+                     CAA => 257,
+                     AVC => 258,
+                     DOA => 259,
+                     TA => 32768,
+                     DLV => 32769);
+   --!pp on
 
    function To_String
      (RR_Type : RR_Types)
@@ -120,8 +192,14 @@ package DNS_Core_Constructs is
       HS,
       QCLASS_NONE,
       QCLASS_ANY);
-   for Classes use (INternet => 1, CH => 3, HS => 4, QCLASS_NONE => 254,
-      QCLASS_ANY             => 255);
+
+   --!pp off
+   for Classes use (INternet => 1,
+                    CH => 3,
+                    HS => 4,
+                    QCLASS_NONE => 254,
+                    QCLASS_ANY => 255);
+   --!pp on
 
    function To_String
      (DNS_Class : Classes)
@@ -148,11 +226,28 @@ package DNS_Core_Constructs is
       BADALG,
       BADTRUNC,
       BADCOOKIE);
-   for RCodes use (NoError => 0, FormErr => 1, ServFail => 2, NXDomain => 3,
-      NotImp => 4, Refused => 5, YXDomain => 6, YXRRSet => 7, NXRRSet => 8,
-      NotAuth => 9, NotZone => 10, BADSIG => 16, BADKEY => 17, BADTIME => 18,
-      BADMODE              => 19, BADNAME => 20, BADALG => 21, BADTRUNC => 22,
-      BADCOOKIE            => 23);
+
+   --!pp off
+   for RCodes use (NoError => 0,
+                   FormErr => 1,
+                   ServFail => 2,
+                   NXDomain => 3,
+                   NotImp => 4,
+                   Refused => 5,
+                   YXDomain => 6,
+                   YXRRSet => 7,
+                   NXRRSet => 8,
+                   NotAuth => 9,
+                   NotZone => 10,
+                   BADSIG => 16,
+                   BADKEY => 17,
+                   BADTIME => 18,
+                   BADMODE => 19,
+                   BADNAME => 20,
+                   BADALG => 21,
+                   BADTRUNC => 22,
+                   BADCOOKIE => 23);
+   --!pp on
 
    type DNS_Packet_Header is record
       Identifier              : Unsigned_16;
@@ -160,7 +255,7 @@ package DNS_Core_Constructs is
       Opcode                  : Unsigned_4;
       Authoritative_Answer    : Boolean;
       Truncated               : Boolean; -- Authoritive Answer
-      Recursion_Desired       : Boolean; --
+      Recursion_Desired       : Boolean;
       Recursion_Available     : Boolean;
       Zero                    : Boolean;
       Authenticated_Data      : Boolean;
